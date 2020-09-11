b8b8e4054b2d82d1dd17bfd6a543085fa684169c
Angelo Romano <angeloromano@Angelos-MacBook-Pro.local>
2020-09-11 22:34:43 +0200

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+var img = document.getElementById('Img');
+
+
+var Dany = {
+    name:['Daenerys Stormborn of the House Targaryen', 'the Unburnt', 'Queen of the Andals and the First Men', 'Khaleesi of the Great Grass Sea', 'Breaker of Chains',  'Mother of Dragons'],
+    age: 21,
+    city: 'valirya',
+    hobby: 'Drakarys',
+    died: 'stabed',
+}
+
+img.onclick = function() {
+    console.log(Dany);
+    // img clicked
+    alert(`${Dany.name}and she was${Dany.age} years old`)
+};
+//document.write(evil);
